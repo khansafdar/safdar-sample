fcb581db4f51df202d73e931cae47284326c2b70
safdar khan <khandimpal012gmail.com>
2019-03-31 12:07:25 +0530

    updated values

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ app.get('/',(req,res)=>{
 app.get('/hi',(req,res)=>{
     res.send('hello' + req.query.name)
 })
+app.listen('/safdar',(req,res)=>{
+    res.send("this is updated version of the project ")
+})
 app.listen(PORT,()=>{
     console.log('server satrted' + PORT)
 })
